4d9b8260421ddcb4c6c0cf74d67102ffd9cb91b7
Igor0501 <s.igor0501@gmail.com>
2023-07-09 22:27:26 +0300

    componentDidUpdate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -20,56 +20,71 @@ export class App extends Component {
     isLoading: false,
   };
 
-  // Якщо оновився стейт рендеримо картинки
+  // If searchQuery or page is updated, fetch new images
   componentDidUpdate(_, prevState) {
-    if (prevState.searchQuery !== this.state.searchQuery) {
+    if (
+      prevState.searchQuery !== this.state.searchQuery ||
+      prevState.page !== this.state.page
+    ) {
       this.getDataImages();
     }
   }
 
-  // При сабміті форми приймає значення інпуту і скидає images та page
- handleSubmitSearchQuery = searchQuery => {
-  if (searchQuery !== this.state.searchQuery) {
-    this.setState({ images: [], searchQuery, page: 1 });
-  }
-};
-
-// Витягуємо дані з фетча і записуємо в стейт
-getDataImages = async () => {
-  const { searchQuery, page } = this.state;
-
-  this.setState({ isLoading: true });
-
-  try {
-    const { hits } = await fetchImages(searchQuery, page);
-
-    this.setState(prevState => ({
-      images: [...prevState.images, ...hits],
-      page: prevState.page + 1, // Збільште номер сторінки на 1 для отримання наступних зображень
-    }));
-
-    if (page !== 1) {
-      scrollOnLoad();
+  // Handle search query submission
+  handleSubmitSearchQuery = searchQuery => {
+    if (searchQuery !== this.state.searchQuery) {
+      this.setState({ images: [], searchQuery, page: 1 });
     }
-  } catch (error) {
-    this.setState({ error: 'Oops something went wrong...' });
-  } finally {
-    this.setState({ isLoading: false });
-  }
-};
+  };
 
-  // Отримуємо Оригінальне зображення по кліку і відкриваємо модалку
+  // Fetch images from the API and update the state
+  getDataImages = async () => {
+    const { searchQuery, page } = this.state;
+
+    this.setState({ isLoading: true });
+
+    try {
+      const response = await fetchImages(searchQuery, page);
+
+      if (response.hits.length === 0) {
+        this.setState({ error: 'Oops. Sorry, but there isn`t anything for this request! Try to find something different!' });
+        this.setState({ isLoading: false, isLoadMoreEnable: false });
+      } else {
+        this.setState(prevState => ({
+          images: [...prevState.images, ...response.hits],
+          isLoadMoreEnable: true,
+        }));
+      }
+
+      if (page !== 1) {
+        scrollOnLoad();
+      }
+    } catch (error) {
+      this.setState({ error: 'Oops something went wrong...' });
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  };
+
+  // Get the large image on click and open the modal
   getLargeImage = largeImage => {
     this.setState({ largeImage, isModalOpen: true });
   };
 
-  // Тогл модалки
+  // Toggle the modal
   toggleShowModal = () => {
     this.setState(({ isModalOpen }) => ({ isModalOpen: !isModalOpen }));
   };
 
   render() {
-    const { images, largeImage, isModalOpen, isLoading, error } = this.state;
+    const {
+      images,
+      largeImage,
+      isModalOpen,
+      isLoading,
+      error,
+      isLoadMoreEnable,
+    } = this.state;
     const lengthImages = images.length >= 12;
 
     return (
@@ -78,14 +93,15 @@ getDataImages = async () => {
         {error}
         <ImageGallery items={images} getItemClick={this.getLargeImage} />
         {isLoading && <Loader />}
-        {lengthImages && <LoadMore onLoadMore={() => this.getDataImages()} />}
+        {lengthImages && isLoadMoreEnable && (
+          <LoadMore onLoadMore={() => this.setState(prevState => ({ page: prevState.page + 1 }))} />
+        )}
         {isModalOpen && (
           <ModalOverlay
             largeImageURL={largeImage}
             onClick={this.toggleShowModal}
           />
         )}
-
         <GlobalStyle />
       </Container>
     );
